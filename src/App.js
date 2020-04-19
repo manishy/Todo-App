cca709e93fe3beb024e023653aab40d79d06dee7
Manish <manishy@thoughtworks.com>
2020-04-19 17:58:35 +0530

    Add todo app with basic functionality.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+const Todo = ({ index, todo }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      {todo.text}
     </div>
   );
-}
+};
+
+const TodoForm = ({addTodo})=>{
+  const [value, setValue] = useState('');
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value);
+    setValue('');
+  };
+  return ( 
+    <form onSubmit ={handleSubmit}>
+      <input type="text" className="input" value={value} onChange={e=>setValue(e.target.value)}/>
+    </form>
+  );
+};
+
+const App = () => {
+  const [toDos, setTodos] = useState([
+    {
+      text: "Have to play cricket",
+      isCompleted: false
+    },
+    {
+      text: "Read book",
+      isCompleted: false
+    },
+    {
+      text: "Cook food",
+      isCompleted: false
+    }]
+  );
+
+  const addTodo= text =>{
+    const newTodos = [... toDos, {text}];
+    setTodos(newTodos);
+  }
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {toDos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />))}
+          <TodoForm addTodo={addTodo}/>
+      </div>
+    </div>
+  )
+};
 
 export default App;
